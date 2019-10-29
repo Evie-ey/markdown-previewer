fbe4c6f4e41af678d798225c545f166292a016af
Evelyne Namwoyo <evelyne@laboremus.no>
2019-10-29 14:34:50 +0100

    Setting up folder structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header">
+        <h1>Hello world</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
